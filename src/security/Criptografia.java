2664c7766505d507a5344340d130cfd48b9cb5b6
pedrochiudini <pedrochiudinil@gmail.com>
2024-11-09 10:56:06 -0300

    Trocando para a camada de segurança da aplicação

---
+++ src/security/Criptografia.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package security;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Criptografia {
+
+    // Método para criptografar a senha
+    public static String criptografarSenha(String senha) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] hash = md.digest(senha.getBytes());
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte b : hash) {
+                hexString.append(String.format("%02x", b));
+            }
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Erro ao criptografar a senha", e);
+        }
+    }
+
+}
